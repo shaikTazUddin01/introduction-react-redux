7198bc0c059b5c290e32172b98a752d231420680
sheikhtazuddin <tazahmedsoft@gmail.com>
2024-06-24 23:35:32 +0600

    create counterSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 function App() {
   return (
-    <>
-      <h1>react redux</h1>
-    </>
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="flex border border-purple-200 p-10 rounded-md bg-slate-100">
+        <button className="px-3 py-2 rounded-md bg-green-500 text-xl font-semibold text-white">Increment</button>
+        <h1 className="text-3xl mx-10">0</h1>
+        <button className="px-3 py-2 rounded-md bg-red-500 text-xl font-semibold text-white">Decrement</button>
+      </div>
+    </div>
   );
 }
 
